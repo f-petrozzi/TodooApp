fc0dc2a6813f2ebac3c88aa4c21c23cda5cf6d5d
Fabrizio <Petrozzi@usf.edu>
2025-06-11 20:15:00 -0400

    Initial commit

---
+++ Todoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todoo
+//
+//  Created by Fabrizio Petrozzi on 6/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todoo/TodooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodooApp.swift
+//  Todoo
+//
+//  Created by Fabrizio Petrozzi on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TodooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
