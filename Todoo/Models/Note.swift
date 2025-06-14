c09f962b4829b238dd4e65096bb46fe724d86437
Fabrizio <Petrozzi@usf.edu>
2025-06-14 13:40:26 -0400

    Add Note model struct to define core data structure for to-do items

---
+++ Todoo/Models/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  Todoo
+//
+//  Created by Fabrizio Petrozzi on 6/14/25.
+//
+import Foundation
+
+struct Note: Identifiable, Codable {
+    var id: Int32
+    var title: String
+    var description: String
+    var date: Date
+    var isCompleted: Bool
+    
+}
