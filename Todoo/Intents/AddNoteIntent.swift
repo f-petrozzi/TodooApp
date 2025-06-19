12a526176d6c4d2438a3b7480729e420e07cb200
Fabrizio <Petrozzi@usf.edu>
2025-06-19 15:45:28 -0400

    Fix Siri prompting for Description to add new notes

--- Todoo/Intents/AddNoteIntent.swift
+++ Todoo/Intents/AddNoteIntent.swift
@@ -8,26 +8,26 @@
 import AppIntents
 
 struct AddNoteIntent: AppIntent {
-    static var title: LocalizedStringResource = "Add a Note"
-    
-    static var description = IntentDescription("Add a new note to Todoo.")
-    
+    static var title = LocalizedStringResource("Add a Note")
+    static var description = IntentDescription("Add a new note to Todoo")
+
     @Parameter(title: "Title")
     var title: String
-    
-    @Parameter(title: "Description", default: "")
-    var description: String
-    
-    //@Parameter(title: "Date")
-    //var date: Date
-    
+
+    @Parameter(title: "Description")
+    var noteDescription: String
+
     static var parameterSummary: some ParameterSummary {
-        Summary("Add a note with \(\.$title) and \(\.$description)")
+        Summary("Add a note titled \(\.$title) with \(\.$noteDescription)")
     }
-    
+
     func perform() async throws -> some IntentResult {
-        let date = Date()
-        let note = Note(title: title, description: description, date: date, isCompleted: false)
+        let note = Note(
+            title: title,
+            description: noteDescription,
+            date: Date(),
+            isCompleted: false
+        )
         DatabaseManager.shared.insertNote(note)
         return .result(value: "Note added to Todoo")
     }
